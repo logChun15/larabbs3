39c711386902eae13328281307fcfd4e1e7d5171
luochun <luochun15@gmail.com>
2022-05-16 21:34:38 +0800

    编辑器

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
     <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    @yield('styles')
 </head>
 
 <body>
@@ -18,9 +19,10 @@
         @include('shared._messages')
         @yield('content')
     </div>
-        @include('layouts._footer')
+    @include('layouts._footer')
     </div>
     <!-- Scripts -->
     <script src="{{ mix('js/app.js') }}"></script>
+    @yield('scripts')
 </body>
 </html>
