a1b8e7c7b0f816a0fb44a2cb1ab6474adce7e0ea
luochun <luochun15@gmail.com>
2022-05-17 21:19:21 +0800

    注册用户默认头像

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -18,4 +18,15 @@ class UserObserver
     {
         //
     }
+
+    public function saving(User $user)
+    {
+        // 这样写扩展性更高，只有空的时候才指定默认头像
+        if (empty($user->avatar)) {
+            // $user->avatar = 'https://iocaffcdn.phphub.org/uploads/images/201710/30/1/TrJS40Ey5k.png';
+            $user->avatar = User::find(1)->avatar; //将用户一的头像作为默认头像
+        }
+    }
+
+
 }
